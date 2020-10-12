4150d513ed2df0bc340819efdf599d2b5e75a3b6
adamerics1119 <72572019+adamerics1119@users.noreply.github.com>
2020-10-12 20:17:00 +0200

    Add files via upload

---
+++ dz1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Domaci 1</title>
+</head>
+<body>
+    <?php echo "Adam <br> Eric <br> S11/2019" ?>
+</body>
+</html>
